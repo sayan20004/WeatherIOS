284523fa3d58a30fea38e18aec2f4172c61fc553
sayan | NotDev <85256833+sayan20004@users.noreply.github.com>
2025-12-11 18:56:51 +0530

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Sayan  Maity  on 11/12/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
